b96286396c2f4499b32a050557bc713417d8486d
mertsigirci11 <62725758+mertsigirci11@users.noreply.github.com>
2022-01-19 20:59:02 +0300

    Add files via upload

---
+++ Deneyap Kodlar/Degiskenler/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    //cin : C + in giriþ iþlemlerinde kullanýlýr
+    //cout: C + out çýkýþ iþlemleri için kullanýlýyor.
+
+    /*
+    int yas;
+    cout << "Yasinizi giriniz: ";
+    cin >> yas;
+    cout << "Yasiniz: "<<yas;
+    */
+
+    /*
+    int sayi1, sayi2;
+    cout<<"Lutfen sayi1 'in degerini giriniz: "<<endl;
+    cin>>sayi1;
+    cout<<"Lutfen sayi2 'nin degerini giriniz: "<<endl;
+    cin>>sayi2;
+    float sonuc;
+    sonuc = (float)(sayi1+sayi2)/2;
+    cout<<"Islemin sonucu: "<<sonuc;
+    */
+
+    /*
+    bool kontrol;
+    kontrol = 5 > 6;
+    cout<<kontrol;
+    */
+
+    /*
+    bool kontrol =true;
+    cout<<kontrol;
+    */
+
+    /*
+    bool kontrol = 1500; //bool veri tipindeki deðiþikenlere 0 deðeri haricinde ne verirsek verelim 1 olarak saklar.
+    cout<<kontrol;
+    */
+
+    /*
+    cout<<a;
+    int a = 20;
+    //Yukarýdan aþaðýya doðru okunduðu için hata verecek çünkü a yukarýda tanýmlanmadý
+    */
+
+    return 0;
+    //system("PAUSE");
+}
+
+
+
+    /*bool kontrol;
+    kontrol = 5 < 4;
+    cout << kontrol;*/
+
+    /*cout << a;
+    int a = 5;*/
+
+    /*bool kontrol = 1;
+    bool kontrol = 0;
+    bool kontrol = -1;
+    bool kontrol = 1531531;
+    cout<<kontrol;*/
